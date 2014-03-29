d2ea5f3a49c3ffe790de5a9a07511240e936ad6b
Alexander Miranda <alexandermichaelmiranda@gmail.com>
2014-03-29 11:07:40 -0700

    Initial commit

---
+++ parans/src/parans.js
@@ -0,0 +1,26 @@
+function paransCount(str) {
+	var total = 0;
+
+	for (var i = 0; i < str.length; i++) {
+		if (total >= 0) {
+			if (str[i] === '\(') {
+				total += 1;
+			}	
+			else if (str[i] === '\)') {
+				total -= 1;	
+			} 
+		}	
+
+		else {
+			return false;
+		}
+	}
+	if (total === 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+module.exports = paransCount;
---
+++ parans/test/parans_spec.js
@@ -0,0 +1,6 @@
+var expect = require('chai').expect,
+	paransCount = require('../src/paransCount.js');
+
+describe('paransCount', function() {
+
+
